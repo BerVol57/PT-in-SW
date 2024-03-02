850dacfab5321968a8d6f0582178499b6b2f9541
Беркута Володимир Юрійович <berkutavolodymyr125@gmail.com>
2024-03-02 12:43:05 +0200

    Lab 1 | again

--- lab1/test/test_d.py
+++ lab1/test/test_d.py
@@ -29,3 +29,4 @@ class TestAssertFalse(unittest.TestCase):
     # test function
     def test_assertFalse_get_diag(self):
         self.assertFalse( get_diag([[2, 2], [1, 1]]) == [1], "How?")
+        

f29e7c75de5f950d46f0449cf6919abaf0714b97
Беркута Володимир Юрійович <berkutavolodymyr125@gmail.com>
2024-03-02 12:45:12 +0200

    Lab 1 | add second assertFalse

--- lab1/test/test_d.py
+++ lab1/test/test_d.py
@@ -29,4 +29,4 @@ class TestAssertFalse(unittest.TestCase):
     # test function
     def test_assertFalse_get_diag(self):
         self.assertFalse( get_diag([[2, 2], [1, 1]]) == [1], "How?")
-        
+        self.assertFalse( get_diag([[0, 0], [0, 0]]) == [1], "Fck wizard")

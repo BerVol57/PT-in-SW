564f153e7041745ece3354c9905733d493b7c344
Беркута Володимир Юрійович <berkutavolodymyr125@gmail.com>
2024-03-02 03:31:55 +0200

    Lab 1 | dumb-code commit

---
+++ lab1/main.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+# check win for tic-tac-toe
+def check_win(matrix: np.ndarray):
+    """
+    if element == 1: its 'X' moves
+    if element == 0: its 'O' moves
+    """
+    winner = 'tie'
+
+    if (np.diag(matrix) == 0).all() or (np.diag(np.fliplr(matrix)) == 0).all():
+        print("Win player [O]")
+        winner = 0
+    elif (np.diag(matrix) == 1).all() or (np.diag(np.fliplr(matrix)) == 1).all():
+        print("Win player [X]")
+        winner = 1
+    else:
+        for i in range(3):
+            if (matrix[i] == 0).all() or (matrix[..., i] == 0).all():
+                print("Win player [O]")
+                winner = 0
+            elif (matrix[i] == 1).all() or (matrix[..., i] == 1).all():
+                print("Win player [X]")
+                winner = 1
+
+    if winner == 'tie':
+        print("Tie")
+        return -1
+    else:
+        return winner
+
+
+if __name__ == "__main__":
+    check_win(np.array([[0, 1, 0], [1, 0, 1], [1, 0, 1]]))

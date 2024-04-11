dbcfffd4f2937a8e057fe9b7e66be1d8d077ea11
Беркута Володимир Юрійович <berkutavolodymyr125@gmail.com>
2024-04-12 01:56:52 +0300

    Lab2 | refactor input data

--- lab2/test.py
+++ lab2/test.py
@@ -6,12 +6,10 @@ from contextlib import redirect_stdout, redirect_stderr
 from eval import *
 
 
-def assert_stdout(self, filepath, expection_stdout, expection_stderr):
+def assert_stdout(self, string_input, expection_stdout, expection_stderr):
     with StringIO() as stdout_buf, StringIO() as stderr_buf:
         with redirect_stdout(stdout_buf), redirect_stderr(stderr_buf):
-            with open(filepath, 'r') as f:
-                input_content = f.read()
-            result = subprocess.run(["py", "eval.py"], input=input_content, text=True, capture_output=True)
+            result = subprocess.run(["py", "eval.py"], input=string_input, text=True, capture_output=True)
         captured_stdout = result.stdout
         captured_stderr = result.stderr
 
@@ -22,10 +20,10 @@ def assert_stdout(self, filepath, expection_stdout, expection_stderr):
             self.assertIn(expection_stderr, captured_stderr)
 
 
-def assert_exit_code(self, filepath, expection_code):
-    with open(filepath, 'r') as f:
-        input_content = f.read()
-    result = subprocess.run(["py", "eval.py"], input=input_content, text=True, capture_output=True)
+def assert_exit_code(self, string_input, expection_code):
+    # with open(filepath, 'r') as f:
+    #     input_content = f.read()
+    result = subprocess.run(["py", "eval.py"], input=string_input, text=True, capture_output=True)
     self.assertEqual(result.returncode, expection_code)
 
 
@@ -51,18 +49,18 @@ class TestStringMethods(unittest.TestCase):
         self.assertFalse(my_eval("2+2)+2)"))
 
     def test_stdin_stdout_and_limit(self):
-        assert_stdout(self, "test_stdin/test1.txt", "6", "")
-        assert_stdout(self, "test_stdin/test_1_number.txt", "456", "")
-        assert_stdout(self, "test_stdin/test_1_symbol.txt", "", "Error")
-        assert_stdout(self, "test_stdin/test_empty.txt", "", "")
-        assert_stdout(self, "test_stdin/test_empty_brackets.txt", "", "Error")
-        assert_stdout(self, "test_stdin/test_maxsize_int.txt", str(sys.maxsize + 1), "")
-        assert_stdout(self, "test_stdin/test_minsize_int.txt", str(-sys.maxsize - 2), "")
+        assert_stdout(self, "2+2*2", "6", "")
+        assert_stdout(self, "456", "456", "")
+        assert_stdout(self, "(", "", "Error")
+        assert_stdout(self, "", "", "")
+        assert_stdout(self, "(             )", "", "Error")
+        assert_stdout(self, "9223372036854775807+1", str(sys.maxsize + 1), "")
+        assert_stdout(self, "-9223372036854775807-2", str(-sys.maxsize - 2), "")
 
     def test_exit_code(self):
-        assert_exit_code(self, "test_stdin/test1.txt", 0)
-        assert_exit_code(self, "test_stdin/test_1_symbol.txt", 1)
-        assert_exit_code(self, "test_stdin/test_empty_brackets.txt", 1)
+        assert_exit_code(self, "2+2*2", 0)
+        assert_exit_code(self, "(", 1)
+        assert_exit_code(self, "(             )", 1)
 
 
 if __name__ == '__main__':
